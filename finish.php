09595e0c8e28973b3e04bc258f54ebf844ba8ec2
humu <masa19951009@gmail.com>
2018-09-09 05:20:03 +0900

    put through finish.php??

---
+++ finish.php
@@ -0,0 +1,69 @@
+<?php
+ini_set("display_errors",1);
+error_reporting(E_ALL);
+
+session_start();
+
+function say_Session(){
+print  $_SESSION['name1'];
+print  $_SESSION['name2'];
+print  $_SESSION['sex'];
+print  $_SESSION['birth'];
+print  $_SESSION['mail'];
+print  $_SESSION['phone'];
+print  $_SESSION['college'];
+print  $_SESSION['subject'];
+print  $_SESSION['hobby'];
+}
+
+
+if (isset($_POST['confirm'])) {
+  $dsn = 'mysql:dbname=Sunseer_BBS; host=localhost; charset=utf8';
+  $user = 'masa';
+  $passwd = 'masa';
+}else {
+  echo "あなたは正規の方法でこのウェブサイトにアクセスしていません。";
+  ?>
+  <br>
+  <a href="#" onclick="javascript:window.history.back(-1);return false;">戻る</a>
+  <?php
+}
+try {
+  $db = new PDO($dsn,$user,$passwd);
+  $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+  var_dump($db);
+  echo "接続に成功しました";
+  $stat = $db->prepare("INSERT INTO db_BBS(id,name1,name2,sex,birth,mail,phone,college,subject,hobby)
+                        VALUES(:id,:name1,:name2,:sex,:birth,:mail,:phone,:college,:subject,:hobby)");
+
+  $stat->bindValue(':id',NULL);
+  $stat->bindValue(':name1',$_SESSION['name1']);
+  $stat->bindValue(':name2',$_SESSION['name2']);
+  $stat->bindValue(':sex',$_SESSION['sex']);
+  $stat->bindValue(':birth',$_SESSION['birth']);
+  $stat->bindValue(':mail',$_SESSION['mail']);
+  $stat->bindValue(':phone',$_SESSION['phone']);
+  $stat->bindValue(':college',$_SESSION['college']);
+  $stat->bindValue(':subject',$_SESSION['subject']);
+  $stat->bindValue(':hobby',$_SESSION['hobby']);
+  $stat->execute();
+
+  if (!($stat->execute())) {
+    echo "eor";
+  }
+
+
+  header("Location:http://".$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']).'/history.php');
+
+} catch (\PDOException $e) {
+print "<br>接続エラー:{$e->getMessage()}";
+say_Session();
+
+
+}finally{
+  $db = null;
+  session_unset();
+  session_destroy();
+}
+
+ ?>
---
+++ input.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>BBS</title>
+</head>
+<body>
+  <h1>BBS</h1>
+  <p>hello</p>
+  <form  action="confirm.php" method="POST">
+    <table border="1">
+      <thead ><th colspan="2">登録</th></thead>
+
+      <tbody>
+
+        <tr>
+        <td>お名前(全角)</td>
+        <td>
+        <label for="firstname">
+          <b>姓:</b>
+        </label>
+          <input type="name" id="firstname" name="firstname" value="">
+
+          <b>例:アース</b>
+          <label for="lastname">
+          <b>名:</b>
+      　  </label>
+          <input width="136.31"　type="name" id="lastname" name="lastname" value="">
+          <b>例:太郎</b>
+      </td>
+        </tr>
+
+
+        <tr>
+        <td>ふりがな(全角)</td>
+        <td>
+        <label for="firstname2">
+          <b>姓:</b>
+        </label>
+          <input type="name" id="firstname2" name="firstname2" value="">
+          <b>例:あーす</b>
+        <label for="lastname2">
+          <b>名:</b>
+        </label>
+          <input width="136.31"　type="name" id="lastname2" name="lastname2" value="">
+          <b>例:たろう</b>
+      </td>
+        </tr>
+
+
+        <tr>
+        <td>性別</td>
+        <td>
+          <label for="male">
+            <b>男</b>
+          </label>
+          <input type="radio" name="sex" id="male"  value="男">
+          <label for="female">
+            <b>女</b>
+          </label>
+            <input type="radio" name="sex" id="female" value="女">
+        </td>
+        </tr>
+
+
+        <tr>
+        <td>生年月日(半角)</td>
+        <td>
+        <input type="date" name="date" value="">
+        <b>例:2018/09/07</b>
+        <!-- <input type="month" name="date" value="">
+        <b>月</b>
+        <input type="day" name="date" value="">
+        <b>日</b> -->
+      </td>
+        </tr>
+
+
+        <tr>
+        <td>連絡可能なメールアドレス(半角)</td>
+        <td>
+          <input type="text" size="30" name="mail" value="" maxlength="30">
+        <b>例:xxxxxx@xxxx.xxx</b>
+         </td>
+        </tr>
+
+
+        <tr>
+        <td>確認入力</td>
+        <td>
+          <input type="text" size="30" name="mailcheck" value="" maxlength="30">
+         </td>
+        </tr>
+
+
+        <tr>
+        <td>連絡可能な電話番号</td>
+        <td>
+          <input type="text" size="20" name="phone" value="" maxlength="15">
+          <b>例:</b>
+         <a href="*">0x0-xxxx-xxxx</a>
+       </td>
+        </tr>
+
+
+        <tr>
+        <td>最終学歴</td>
+        <td>
+          <label for="college">
+          <b>学校名:</b>
+        </label>
+          <input type="text" name="college" id="college" value="">
+          <b>例:東京大学</b>
+          <br>
+          <label for="subject">
+          <b>学部・学科・科名</b>
+        </label>
+          <input type="text" name="subject" id="subject" value="">
+          <br>
+       </td>
+        </tr>
+
+
+        <tr>
+        <td>特技</td>
+        <td>
+          <textarea name="hobby" value=""></textarea>
+        </td>
+        </tr>
+
+
+        <tr>
+        <td>確認-リセット</td>
+        <td align="center">
+          <input type="submit" value="確認">
+          <input type="reset">
+        </td>
+        </tr>
+
+</tbody>
+</table>
+</form>
+
+
+
+</body>
+</html>
